f4b4fe9222a6bb8b1edb3b6daa24c4d93946c835
Eric Newman <newmaneric5@gmail.com>
2019-02-19 09:20:26 -0600

    initial commit

---
+++ desktop/src/com/libgdxtest/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.libgdxtest.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.libgdxtest.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
